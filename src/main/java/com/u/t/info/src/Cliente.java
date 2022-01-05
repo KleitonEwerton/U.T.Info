933bc7b7a0dad5106433204fa6fd4766f6d5292b
Nikolas <nikolas.genesio@estudante.ufjf.br>
2022-01-05 14:09:53 -0300

    Cliente

---
+++ src/main/java/com/u/t/info/src/Cliente.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.u.t.info.src;
+
+public class Cliente {
+    private String nome;
+    private String endereco;
+    private String cpf;
+    private String contato;
+
+    public Cliente(String nome, String endereco, String cpf, String contato) {
+        this.nome = nome;
+        this.endereco = endereco;
+        this.cpf = cpf;
+        this.contato = contato;
+    }
+    
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getContato() {
+        return contato;
+    }
+
+    public void setContato(String contato) {
+        this.contato = contato;
+    }
+}
