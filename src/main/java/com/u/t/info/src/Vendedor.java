b8a1c3fe202a7ebcd2bf8f4423f4b5412593b1c7
Luizmbs <luiz2001mbs@gmail.com>
2022-01-05 19:08:12 -0300

    Adicionando a classe Vendedor

---
+++ src/main/java/com/u/t/info/src/Vendedor.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.u.t.info.src;
+
+/**
+ *
+ * @author luiz
+ */
+public class Vendedor extends Funcionario {
+    
+    public Vendedor(String nome, String contato, String cpf, String codigoHash, Integer identificacao,
+			String endereço)
+    {
+        super(nome,contato,cpf,codigoHash,identificacao,endereço);
+    }
+    
+    public void cadatrarCliente(String nome, String endereco, String cpf, String contato)
+    {
+        Cliente cliente = new Cliente(nome,endereco,cpf,contato);
+        System.out.println(nome + " foi cadastrado(a).");
+    }
+    
+    public void pedidoDevolucao(Produto produto, String causa)
+    {
+        System.out.println("O produto " + produto.getNome() + " vai ser analisado.");
+        System.out.println("Causa da devolução: " + causa);
+    }
+    
+    public void efetuarVenda(Produto produto, Cliente cliente)
+    {
+        produto.setQtdeEstoque(produto.getQtdeEstoque()-1);
+        System.out.println("Venda do " + produto.getNome() + " para " + cliente.getNome());
+    }
+}
